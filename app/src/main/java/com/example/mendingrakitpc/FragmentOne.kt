d5555f2169923bfb623b424a5d0379bccd9f8ae3
Miaw Fin <cpt.miaw1060@gmail.com>
2020-12-29 17:03:04 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mendingrakitpc/FragmentOne.kt
@@ -0,0 +1,11 @@
+package com.example.mendingrakitpc
+
+import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+
+class FragmentOne : FragmentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_one_1)
+    }
+}
