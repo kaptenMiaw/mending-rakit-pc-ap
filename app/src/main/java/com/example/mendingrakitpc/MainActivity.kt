9aab9d64bb5381b6d432cc2b580cb192a78ff18d
Miaw Fin <cpt.miaw1060@gmail.com>
2020-12-29 17:42:19 +0700

    adding btn to undi prize

--- app/src/main/java/com/example/mendingrakitpc/MainActivity.kt
+++ app/src/main/java/com/example/mendingrakitpc/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.mendingrakitpc
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 
 class MainActivity : FragmentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val undiBtn = findViewById<Button>(R.id.undi_btn)
+        undiBtn.setOnClickListener {
+            undiHasil()
+        }
+    }
+
+    private fun undiHasil() {
+        val undiBtn = findViewById<TextView>(R.id.hasil_undi)
+        val hasilUndi = java.util.Random().nextInt(6)+1
+        undiBtn.text = hasilUndi.toString()
     }
 }
