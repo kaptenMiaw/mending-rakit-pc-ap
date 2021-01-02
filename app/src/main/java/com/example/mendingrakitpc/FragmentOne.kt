a4f79f4187a0cf5fbda8aaa963adb89027fd37be
Miaw Fin <cpt.miaw1060@gmail.com>
2021-01-02 14:31:09 +0700

    make new logo for ic launcher and make a color game in fragment_one_1.xml

--- app/src/main/java/com/example/mendingrakitpc/FragmentOne.kt
+++ app/src/main/java/com/example/mendingrakitpc/FragmentOne.kt
@@ -3,6 +3,7 @@ package com.example.mendingrakitpc
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.FragmentActivity
 
@@ -15,13 +16,21 @@ class FragmentOne : FragmentActivity() {
     }
 
     private fun setListener() {
+        val headerText = findViewById<TextView>(R.id.header_title)
+        val pcText = findViewById<TextView>(R.id.pc_list)
         val boxOneText = findViewById<TextView>(R.id.desc_one)
         val boxTwoText = findViewById<TextView>(R.id.desc_two)
         val boxThreeText = findViewById<TextView>(R.id.desc_three)
+
         val rootConstraintLayout = findViewById<View>(R.id.constraint_layout)
 
+        val btnOne = findViewById<Button>(R.id.btn_one)
+        val btnTwo = findViewById<Button>(R.id.btn_two)
+        val btnThree = findViewById<Button>(R.id.btn_three)
+
         val clickText: List<View> =
-                listOf(boxOneText,boxTwoText,boxThreeText,rootConstraintLayout)
+                listOf(boxOneText,boxTwoText,boxThreeText,rootConstraintLayout,
+                    btnOne,btnTwo,btnThree,pcText,headerText)
         for(items in clickText){
             items.setOnClickListener{makeColored(it)}
         }
@@ -31,7 +40,12 @@ class FragmentOne : FragmentActivity() {
         when(view.id){
             R.id.desc_one -> view.setBackgroundColor(Color.CYAN)
             R.id.desc_two -> view.setBackgroundColor(Color.BLUE)
-            R.id.desc_three -> view.setBackgroundColor(Color.YELLOW)
+            R.id.desc_three -> view.setBackgroundColor(Color.GREEN)
+            R.id.pc_list -> view.setBackgroundResource(R.drawable.dice_1)
+            R.id.header_title -> view.setBackgroundColor(Color.RED)
+            R.id.btn_one -> view.setBackgroundColor(Color.BLUE)
+            R.id.btn_two -> view.setBackgroundColor(Color.MAGENTA)
+            R.id.btn_three -> view.setBackgroundColor(Color.RED)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
